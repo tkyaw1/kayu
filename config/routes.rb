65f037faa711c348cb2820628dad3ecf42a81043
tkyaw1 <theintthinzark21@gmail.com>
2017-05-18 00:15:06 -0700

    mostly static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
 
   get 'static_pages/home'
 
@@ -6,7 +7,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/contact'
 
-  root 'application#hello'
 end
